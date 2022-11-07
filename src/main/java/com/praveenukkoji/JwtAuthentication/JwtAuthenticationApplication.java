422f42c94bc140847ce54d66ec64a6d0bde739c5
praveenukkoji <praveenukkoji@gmail.com>
2022-11-07 12:49:49 +0530

    api package

--- src/main/java/com/praveenukkoji/JwtAuthentication/JwtAuthenticationApplication.java
+++ src/main/java/com/praveenukkoji/JwtAuthentication/JwtAuthenticationApplication.java
@@ -1,7 +1,14 @@
 package com.praveenukkoji.JwtAuthentication;
 
+import com.praveenukkoji.JwtAuthentication.Model.AppUser;
+import com.praveenukkoji.JwtAuthentication.Model.Role;
+import com.praveenukkoji.JwtAuthentication.Service.AppUserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class JwtAuthenticationApplication {
@@ -10,4 +17,34 @@ public class JwtAuthenticationApplication {
 		SpringApplication.run(JwtAuthenticationApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner runner(AppUserService userService) {
+		return args -> {
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_MANAGER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+			userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+			userService.saveUser(new AppUser(null, "Praveen", "praveen", "praveen",
+					new ArrayList<>()));
+			userService.saveUser(new AppUser(null, "Prem", "prem", "prem",
+					new ArrayList<>()));
+			userService.saveUser(new AppUser(null, "Pratik", "pratik", "pratik",
+					new ArrayList<>()));
+			userService.saveUser(new AppUser(null, "Rohit", "rohit", "rohit",
+					new ArrayList<>()));
+
+			userService.addRoleToAppUser("praveen", "ROLE_SUPER_ADMIN");
+			userService.addRoleToAppUser("praveen", "ROLE_ADMIN");
+			userService.addRoleToAppUser("praveen", "ROLE_MANAGER");
+			userService.addRoleToAppUser("praveen", "ROLE_USER");
+
+			userService.addRoleToAppUser("prem", "ROLE_ADMIN");
+
+			userService.addRoleToAppUser("pratik", "ROLE_MANAGER");
+
+			userService.addRoleToAppUser("rohit", "ROLE_USER");
+
+		};
+	}
 }
