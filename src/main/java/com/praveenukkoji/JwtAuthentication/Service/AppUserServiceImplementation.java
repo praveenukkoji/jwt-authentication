f7a02e294bd3bd3749b582c15283c4a4f4c6d33e
praveenukkoji <praveenukkoji@gmail.com>
2022-11-08 13:33:20 +0530

    jwt token added (authentication & authorization)

--- src/main/java/com/praveenukkoji/JwtAuthentication/Service/AppUserServiceImplementation.java
+++ src/main/java/com/praveenukkoji/JwtAuthentication/Service/AppUserServiceImplementation.java
@@ -6,23 +6,48 @@ import com.praveenukkoji.JwtAuthentication.Repository.RoleRepository;
 import com.praveenukkoji.JwtAuthentication.Repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 @Slf4j
-public class AppUserServiceImplementation implements AppUserService{
+public class AppUserServiceImplementation implements AppUserService, UserDetailsService {
 
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser user = userRepository.findByUsername(username);
+        if(user == null) {
+            log.error("User not found in database");
+            throw new UsernameNotFoundException("User not found in database");
+        }
+        log.info("User found in database: {}", user);
+
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.getRolename()));
+        });
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
+    }
 
     @Override
     public AppUser saveUser(AppUser user) {
         log.info("Saving new user {} to the database", user.getName());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
